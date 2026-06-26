593955dc2cc9b969023d8ec9cb6d0ac56c5129f4
Atharva Dalvi <atharvadal7@gmail.com>
2026-06-26 09:51:05 +0800

    Add private subnets across two AZs

--- networking/main.tf
+++ networking/main.tf
@@ -53,3 +53,23 @@ resource "aws_vpc_endpoint" "s3" {
     Name = "learning-s3-endpoint"
   }
 }
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "learning-private-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "learning-private-b"
+  }
+}
